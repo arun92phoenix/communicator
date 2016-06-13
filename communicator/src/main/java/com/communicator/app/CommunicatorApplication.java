547c361a07ebc28914bd2c7451ab7a6b2de80fea
Pavan Andhukuri <pavan.kumar@darkhorseboa.com>
2016-06-14 00:22:20 +0530

    Initial Commit

---
+++ communicator/src/main/java/com/communicator/app/CommunicatorApplication.java
@@ -0,0 +1,12 @@
+package com.communicator.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicatorApplication.class, args);
+	}
+}
